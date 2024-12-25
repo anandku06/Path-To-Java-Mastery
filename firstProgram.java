9775ce4c4bb86d53529541fa8f2711662c75a941
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-25 23:19:41 +0530

    Add comments to firstProgram.java explaining Java basics and program structure

--- firstProgram.java
+++ firstProgram.java
@@ -1,5 +1,22 @@
+// package com.company -> A package in Java is used to group related classes. Think of it as a folder in a file directory
+
 public class firstProgram{
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Starting-point function
         System.out.println("Hello World!!");
     }
 }
+
+/* basically 'public static void main' is like entry point of the execution for the Java program 
+    - public -> an access modifier ; this methods can be accessed publically
+    - static keyword -> without making any instance(object) of the class, the class's method can be used.
+    - void keyword -> return type of the function ; specifies that the function won't return a thing
+
+We made a class named 'firstProgram' with the same file name and it has a function 'main' and this all is grouped under the package 'com.company'
+
+Basic Structure of a Java Program -> made of two things
+- Functions : A piece of code that does a specific task like addition, subtraction
+
+- Classes : A collection of similar type of methods(functions) or properties that represents a group
+    works as a template for making objects
+
+*/
