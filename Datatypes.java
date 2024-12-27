bf0352f5b054f9f07379e80421b9d1df31667aae
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-27 23:27:59 +0530

    Add documentation for data types in Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -1,3 +1,12 @@
+/* 
+    DataTypes in Java are of two types:
+    - Primitive Datatypes (Intrinsic)
+    - Non-Primitive Datatypes (Derived)
+
+    Primitive Datatypes:
+        Java is statically typed i.e. variables must be declared before use!!
+*/
+
 public class Datatypes {
     public static void main(String[] args) {
         
