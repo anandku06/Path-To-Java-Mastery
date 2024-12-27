bade5c0b07c4ec080914f0af1aa65a9aba0452ca
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-27 22:25:52 +0530

    Add Variables.java with initial variable declaration and explanation

---
+++ Variables.java
@@ -0,0 +1,11 @@
+/* Variables are containers that stores a value. This value can be changed during the execution of the program.
+    Syntax:
+    datatype variableName = value; // '=' is called assignment operator. assigns value to any variable
+*/
+
+
+public class Variables {
+    public static void main(String[] args) {
+
+    }
+}
