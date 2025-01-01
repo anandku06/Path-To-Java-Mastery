aefe9a0db42ad9e3ab8cadeff74dfbfa493c5b72
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-01 23:32:19 +0530

    Add comments to MyFirstProgram.java explaining class structure and main method

--- BroCode_Java/MyFirstProgram.java
+++ BroCode_Java/MyFirstProgram.java
@@ -1,5 +1,11 @@
+/* In Java, all the code is written inside a class and the name of
+ the class is same as the name of the java file.
+ In the class declaration, there is a main method from which the
+ program execution starts.
+*/
+
 public class MyFirstProgram {
-    public static void main(String args[]){
+    public static void main(String[] args){
 
     }
 }
