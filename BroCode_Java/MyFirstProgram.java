d35a2f375c486d23e20379d380bad93e0706590f
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-01 23:46:58 +0530

    Update MyFirstProgram.java to enhance output and add comments

--- BroCode_Java/MyFirstProgram.java
+++ BroCode_Java/MyFirstProgram.java
@@ -2,10 +2,20 @@
  the class is same as the name of the java file.
  In the class declaration, there is a main method from which the
  program execution starts.
+
+ Every code line in Java must end with semi-colon(;)
 */
 
 public class MyFirstProgram {
     public static void main(String[] args){
-        System.err.print("Hello World!!"); // used to print anything in the parenthesis (in the same line)
+        System.err.println("Hello World!!"); // used to print anything to the console (in new line)
+        System.out.print("I am Robot.\n"); // used to print anything in the parenthesis (in the same line)
+        System.out.print("BANKAI \n Katen Kyokutsu Karamtsu Shinjo"); // '\n' is a escape sequence character used to tell the compiler to shift to a new line
+        System.out.print("\nBANKAI \n Tensa Zangetsu");
     }
 }
+
+// this is single-line commect
+/* this is a multi-line comment : Comments are for programmers,
+    especially used for taking notes which is ignored by the compiler
+ */
