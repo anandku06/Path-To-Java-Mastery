7c88939b2c7365bbb292200f64e85479d5f309c5
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-02 00:02:02 +0530

    Add detailed comments to Variables.java explaining variable types and declaration process

--- BroCode_Java/Variables.java
+++ BroCode_Java/Variables.java
@@ -1,3 +1,21 @@
+/* 
+    Variable -> a re-usable container for a value;
+        a variable behaves as if it was the value it contains
+
+    Two types of variables:
+    - Primitive -> simple value stored directly in memory (stack)
+    - Reference -> memory address (stack) that points to the heap.
+
+    Primitive examples -> int, double, float, char, boolean
+    Reference examples -> string, array, object
+
+    2 steps to declare any variable -
+        1. declaration
+        2. assignment
+*/
+
 public class Variables {
-    
+    public static void main(String[] args) {
+
+    }
 }
