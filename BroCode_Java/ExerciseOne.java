307d599bcdc1b6224d9d1f318dec5d3911585f28
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-04 00:07:11 +0530

    Implement area calculation for a rectangle in ExerciseOne class

--- BroCode_Java/ExerciseOne.java
+++ BroCode_Java/ExerciseOne.java
@@ -6,5 +6,17 @@ public class ExerciseOne {
         // Calculate Area of a rectangle
 
         Scanner scanner = new Scanner(System.in); // 'System.in' is a standard input stream in Java; by default it represents the keyboard input; It is of type 'InputStream' and is provided by the 'java.lang' package
+
+        double width, length, area; // here declared variables, need not to be declared again; only initialised with a value
+
+        System.out.print("Enter the length: ");
+        length = scanner.nextDouble();
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        area = length * width;
+
+        System.out.println("The Area of the rectangle of length " + length + " m and width " + width + " m is: " + area + " m.");
     }
 }
