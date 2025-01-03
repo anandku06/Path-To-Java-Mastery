d3ad4cfa8a8f8cff8e75d29f3254017f04089144
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-03 23:03:30 +0530

    Enhance UserInput class to collect additional user information and provide scholarship eligibility feedback

--- BroCode_Java/UserInput.java
+++ BroCode_Java/UserInput.java
@@ -13,8 +13,24 @@ public class UserInput {
         System.out.print("Enter your name: ");
         String name = scanner.next(); // used to take the input until any white space is encountered
         // String name = scanner.nextLine(); // takes input from the user until newline is encountered
-        
+        System.out.print(", your age: ");
+        int age = scanner.nextInt(); // used to take integer inputs only from the user; if other datatypes are entered then throws error
+        System.out.print("and your CGPA: ");
+        double cgpa = scanner.nextDouble();
+
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean(); // used to take boolean values as input from the user
+
         System.out.println("Hello " + name);
+        System.out.println("Your age is " + age + " years");
+        System.out.println("Your CGPA is " + cgpa);
+
+        if(isStudent){
+            System.out.println("You can apply for scholarship");
+        }
+        else{
+            System.out.println("You can't apply for any scholarship");
+        }
         scanner.close(); // closing working of the scanner object to avoid any unexpected behaviour
     }
 }
