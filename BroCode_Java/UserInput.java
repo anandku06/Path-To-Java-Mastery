c3d1154f9dc6d66c3578766031261881796c4b09
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-03 22:50:01 +0530

    Update UserInput class to enhance user greeting and clarify input handling

--- BroCode_Java/UserInput.java
+++ BroCode_Java/UserInput.java
@@ -11,9 +11,10 @@ public class UserInput {
         Scanner scanner = new Scanner(System.in); // made a instance of Scanner class using 'new' keyword
 
         System.out.print("Enter your name: ");
-        String name = scanner.next();
+        String name = scanner.next(); // used to take the input until any white space is encountered
+        // String name = scanner.nextLine(); // takes input from the user until newline is encountered
         
-        System.out.println("The provided input is: " + name);
+        System.out.println("Hello " + name);
         scanner.close(); // closing working of the scanner object to avoid any unexpected behaviour
     }
 }
