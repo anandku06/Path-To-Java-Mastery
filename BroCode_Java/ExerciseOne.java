4d01de2210058241118d1226554ae490b1145f54
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-03 23:25:03 +0530

    Add Scanner import to ExerciseOne class for user input handling

--- BroCode_Java/ExerciseOne.java
+++ BroCode_Java/ExerciseOne.java
@@ -1,5 +1,10 @@
+
+import java.util.Scanner;
+
 public class ExerciseOne {
     public static void main(String[] args){
         // Calculate Area of a rectangle
+
+        Scanner scanner = new Scanner(System.in); // 'System.in' is a standard input stream in Java; by default it represents the keyboard input; It is of type 'InputStream' and is provided by the 'java.lang' package
     }
 }
