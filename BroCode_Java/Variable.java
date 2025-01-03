cd8ef2d018dbb06733911545bf135e8e6996a280
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-03 17:39:04 +0530

    Add boolean variable and conditional statements to Variable class for user login feedback

--- BroCode_Java/Variable.java
+++ BroCode_Java/Variable.java
@@ -26,12 +26,25 @@ public class Variable {
 
         double interest = 0.11; // bcz the decimal numbers without any literals are considered as double
 
+        boolean isLoggedIn = true; // boolean variable takes only boolean values i.e. true or false
+        // this type of casing is known as camelCasing.
+
         System.out.println(age);
         System.out.println(letter);
         System.out.println(rate);
         System.out.println(interest);
+        System.out.println(isLoggedIn);
         System.out.println("The current interest on the Loan is " + interest); // this is concatenation of strings using '+' operator
         System.out.println("The current interest on the Loan is " + "interest"); // this is concatenation of strings using '+' operator ; you can give any other string
         // note we are not adding any thing to the interest value or the sentence written its just printed together by this method
+
+        // more practical use of boolean values is internally by if-else statements
+
+        if(isLoggedIn){ // here the condition in the parenthesis is checked; 'if' true then the statments under if block is executed
+            System.out.println("Welcome User");
+        }
+        else{ // 'if' block is false so this block is executed
+            System.out.println("New User, Please Login!!");
+        }
     }
 }
