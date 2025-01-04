020457bc6250293039659d886d96bc7cc80f41cc
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 00:19:26 +0530

    Refactor Operators class comments and demonstrate modulus operator usage

--- BroCode_Java/Operators.java
+++ BroCode_Java/Operators.java
@@ -1,14 +1,21 @@
-// Arithmetic Operators in Java
+// Operators in Java
 
 public class Operators {
     public static void main(String[] args){
+        // Arithmetic Operators
 
         int x = 10;
         int y = 2;
         int z;
 
-        z = x + y;
+        // z = x + y; // Addition
+        // z = x - y; // Subtraction
+        // z = x * y; // Multiplication
+        // z = x / y; // Division
+        z = x % y; // Modulus Operator : returns the remainder after the division
 
         System.out.println(z);
+
+        // Augmented Assignment Operators
     }   
 }
