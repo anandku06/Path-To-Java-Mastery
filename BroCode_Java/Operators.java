48d83da8d58476d2ea84cd1fc9ea46da3541d3b8
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 00:11:04 +0530

    Add main method to Operators class for arithmetic operation demonstration

--- BroCode_Java/Operators.java
+++ BroCode_Java/Operators.java
@@ -1,3 +1,14 @@
+// Arithmetic Operators in Java
+
 public class Operators {
-    
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y;
+
+        System.out.println(z);
+    }   
 }
