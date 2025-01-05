f11aae77dfe135842c6a1420d08958d1ae6edc9b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 20:45:09 +0530

    Refactor age check logic in If_Else class to include senior age category and improve comments

--- BroCode_Java/If_Else.java
+++ BroCode_Java/If_Else.java
@@ -1,4 +1,7 @@
-
+/*
+    Comparison Operator :-
+    >, <, <=, >=, !=, == -> used to compare any two values and returns true/false according to the comparison 
+*/
 import java.util.Scanner;
 
 public class If_Else {
@@ -7,12 +10,17 @@ public class If_Else {
         // if the condition is true, then the code in the if block is executed ; if 'false', then the else block is executed
         // if there are more than one condition to check then we use 'else-if' block to handle that condition
 
+        // note: the conditions are checked from top-bottom; i.e. if the 'if' and 'else-if' both are correct then the 'if' block will execute rather than the 'else-if' block ; keep a track of the positioning of your if-else condtions
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your age!! -> ");
         int age = scanner.nextInt();
 
-        if(age >= 18){ // checks the condition
-            System.out.println("You are an adult");
+        if(age >= 65){ // checks the condition
+            System.out.println("You are an senior");
+        }
+        else if(age >= 18){ // if age is greater than 65 then also the 'if' block will be executed
+            System.out.println("You are a adult");
         }
         else if(age <= 0){ // if the 'if' block is not executed
             System.out.println("You haven't been born yet");
@@ -20,5 +28,7 @@ public class If_Else {
         else{// if the condition is false then execute this
             System.out.println("You are not an adult");
         }
+
+        scanner.close();
     }
 }
