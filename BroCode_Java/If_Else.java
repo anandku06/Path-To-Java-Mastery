f120370936a59224959bc106767fc061e6f9f09b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 20:53:46 +0530

    Add name input and validation to If_Else class

--- BroCode_Java/If_Else.java
+++ BroCode_Java/If_Else.java
@@ -13,6 +13,20 @@ public class If_Else {
         // note: the conditions are checked from top-bottom; i.e. if the 'if' and 'else-if' both are correct then the 'if' block will execute rather than the 'else-if' block ; keep a track of the positioning of your if-else condtions
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name -> ");
+        String name = scanner.nextLine();
+
+        if(name.isEmpty()){
+            System.out.println("Please enter your name!!");
+        } // a String method used to check whether the String is empty or not
+        // can use '.isBlank()' -> Returns true if the string is empty or contains only white space codepoints, otherwise false.
+        else{
+            System.out.println("Hello " + name + "!!!");
+        }
+
+
+
         System.out.print("Enter your age!! -> ");
         int age = scanner.nextInt();
 
@@ -20,7 +34,7 @@ public class If_Else {
             System.out.println("You are an senior");
         }
         else if(age >= 18){ // if age is greater than 65 then also the 'if' block will be executed
-            System.out.println("You are a adult");
+            System.out.println("You are an adult");
         }
         else if(age <= 0){ // if the 'if' block is not executed
             System.out.println("You haven't been born yet");
