b7f080557ac04f321b69d3f813d09d67640ba171
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 20:29:03 +0530

    Implement main method in If_Else class with age check and conditional statements

--- BroCode_Java/If_Else.java
+++ BroCode_Java/If_Else.java
@@ -1,3 +1,15 @@
 public class If_Else {
-    
+    public static void main(String[] args){
+        // if-statement -> peforms a block of code if its condition is true
+        // if the condition is true, then the code in the if block is executed ; if 'false', then the else block is executed
+
+        int age = 25;
+
+        if(age >= 18){ // checks the condition
+            System.out.println("You are an adult");
+        }
+        else{// if the condition is false then execute this
+            System.out.println("You are not an adult");
+        }
+    }
 }
