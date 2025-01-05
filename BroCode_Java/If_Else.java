f109344ea5a324864471c6a236071feab0aa78b3
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-05 20:31:47 +0530

    Add else-if condition to age check in If_Else class

--- BroCode_Java/If_Else.java
+++ BroCode_Java/If_Else.java
@@ -2,11 +2,15 @@ public class If_Else {
     public static void main(String[] args){
         // if-statement -> peforms a block of code if its condition is true
         // if the condition is true, then the code in the if block is executed ; if 'false', then the else block is executed
+        // if there are more than one condition to check then we use 'else-if' block to handle that condition
 
         int age = 25;
 
         if(age >= 18){ // checks the condition
             System.out.println("You are an adult");
+        }
+        else if(age < 0){ // if the 'if' block is not executed
+            System.out.println("You haven't been born yet");
         }
         else{// if the condition is false then execute this
             System.out.println("You are not an adult");
