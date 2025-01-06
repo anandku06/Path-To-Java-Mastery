9e1db505f1e05fe1c37c17b47c40fa2f3f6beea1
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-06 22:59:55 +0530

    Add additional random number generation methods in RandomClass

--- BroCode_Java/RandomClass.java
+++ BroCode_Java/RandomClass.java
@@ -9,9 +9,15 @@ public class RandomClass {
         Random random = new Random();
 
         int num = random.nextInt(6); // generates random number upto 6 (exclusive)
-        // it generates number that is very large between the range of the 'int' datatype
+        // if not given any args : it generates number that is very large between the range of the 'int' datatype
         // to avoid this, give the terminating range.
 
+        double num1 = random.nextDouble(); // generates random number between 0 and 1
+
+        boolean isHeads = random.nextBoolean(); // generates true or false randomly
+
         System.out.println(num);
+        System.out.println(num1);
+        System.out.println(isHeads);
     }
 }
