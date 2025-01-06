bebe98b6c8c70922f651a32ed4984a50ba5757eb
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-06 22:24:23 +0530

    Add random number generation example to RandomClass

--- BroCode_Java/RandomClass.java
+++ BroCode_Java/RandomClass.java
@@ -1,3 +1,17 @@
+
+import java.util.Random;
+
+/* Random number generation in Java using Random Class ; 
+    To use the Random class just like Scanner class make its instance and use its properties and methods */ 
+
 public class RandomClass {
-    
+    public static void main(String[] args){
+        Random random = new Random();
+
+        int num = random.nextInt(1, 6); 
+        // it generates number that is very large between the range of the 'int' datatype
+        // to avoid this, give range as its params.
+
+        System.out.println(num);
+    }
 }
