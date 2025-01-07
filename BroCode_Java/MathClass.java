35b2edbb65ea29a2c9e5836bd76671619073f93d
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 18:19:25 +0530

    Add methods to calculate absolute value and square root in MathClass

--- BroCode_Java/MathClass.java
+++ BroCode_Java/MathClass.java
@@ -8,6 +8,11 @@ public class MathClass {
 
         double result = Math.pow(2, 3); // returns the power raised value of the given args ; first is the base and second is the exponent
 
+        int res = Math.abs(-5); // returns the absolute value of the given number
+
+        result = Math.sqrt(64); // returns the sqaure root of the given number ; returns double value
+
         System.out.println(result);
+        System.out.println(res);
     }
 }
