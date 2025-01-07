a1fe1e706e655f443ffd27e5bc85ec2e00770bf3
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 18:15:41 +0530

    Add main method to MathClass for demonstrating mathematical constants and operations

--- BroCode_Java/MathClass.java
+++ BroCode_Java/MathClass.java
@@ -1,3 +1,13 @@
+// Math class in Java
+
 public class MathClass {
-    
+    public static void main(String[] args) {
+        System.out.println(Math.PI); // returns the pi's value
+
+        System.out.println(Math.E); // returns the Euler's value
+
+        double result = Math.pow(2, 3); // returns the power raised value of the given args ; first is the base and second is the exponent
+
+        System.out.println(result);
+    }
 }
