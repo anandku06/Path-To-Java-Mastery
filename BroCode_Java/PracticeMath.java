fcb0eebca42ef5f55ddcfb0a4eab7e306b74217b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 18:43:28 +0530

    Add main method to PracticeMath for calculating triangle hypotenuse

--- BroCode_Java/PracticeMath.java
+++ BroCode_Java/PracticeMath.java
@@ -1,3 +1,24 @@
+
+import java.util.Scanner;
+
 public class PracticeMath {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a, b, c;
+
+        System.out.print("Enter the first side of the triangle -> ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter the second side of the triangle -> ");
+        b = scanner.nextDouble();
+
+        c = Math.hypot(a, b); // either this
+        double c1 = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)); // or this both are correct
+
+        System.out.println("The hypotenuse of the triangle is -> " + c + " m");
+        System.out.println("The hypotenuse of the triangle is -> " + c1 + " m");
+
+        scanner.close();
+    }
 }
