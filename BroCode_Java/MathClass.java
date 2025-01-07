7c50edead79dc9d42cd12f527adbba002cd04337
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 18:34:11 +0530

    Enhance MathClass with additional rounding and ceiling/floor methods

--- BroCode_Java/MathClass.java
+++ BroCode_Java/MathClass.java
@@ -8,9 +8,15 @@ public class MathClass {
 
         double result = Math.pow(2, 3); // returns the power raised value of the given args ; first is the base and second is the exponent
 
-        int res = Math.abs(-5); // returns the absolute value of the given number
+        double res = Math.abs(-5); // returns the absolute value of the given number
 
-        result = Math.sqrt(64); // returns the sqaure root of the given number ; returns double value
+        result = Math.sqrt(64); // returns the square root of the given number ; returns double value
+
+        result = Math.round(3.14); // rounds the number to thier nearest integer value
+
+        res = Math.ceil(3.14); // returns the smallest value ranging from the number given to the infinity
+
+        res = Math.floor(3.14); // returns the largest value ranging from the -infinity to the number given 
 
         System.out.println(result);
         System.out.println(res);
