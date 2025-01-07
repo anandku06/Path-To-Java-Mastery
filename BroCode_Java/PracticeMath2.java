8a86ef5292fea4e4a681732e1e56ce67d2b4a92a
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 19:33:23 +0530

    Add calculations for area and volume of the sphere in PracticeMath2

--- BroCode_Java/PracticeMath2.java
+++ BroCode_Java/PracticeMath2.java
@@ -11,8 +11,12 @@ public class PracticeMath2 {
         radius = scanner.nextDouble();
 
         circumference = 2 * Math.PI * radius;
-
-        System.out.println("The circumference of the sphere is -> " + circumference);
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+ 
+        System.out.println("The circumference of the sphere is -> " + circumference + "m");
+        System.out.println("The area of the sphere is -> " + area + "m²");
+        System.out.println("The volume of the sphere is -> " + volume + "m³");
 
         scanner.close();
     }
