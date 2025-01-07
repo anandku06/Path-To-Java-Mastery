4c361ffcbb8e2e987d3fd33d461a80d3ebfacc46
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-07 19:26:11 +0530

    Add main method to PracticeMath2 for calculating sphere circumference

--- BroCode_Java/PracticeMath2.java
+++ BroCode_Java/PracticeMath2.java
@@ -1,3 +1,19 @@
+
+import java.util.Scanner;
+
 public class PracticeMath2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double radius, circumference, area, volume;
+
+        System.out.print("Enter the radius -> ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+
+        System.out.println("The circumference of the sphere is -> " + circumference);
+
+        scanner.close();
+    }
 }
