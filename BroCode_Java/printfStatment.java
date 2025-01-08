bf67d197f84c88057303af2ef28c3bd9d75f07f2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-08 17:37:10 +0530

    Add formatted output example in printfStatment class with explanations for printf usage

--- BroCode_Java/printfStatment.java
+++ BroCode_Java/printfStatment.java
@@ -1,5 +1,15 @@
 public class printfStatment {
     public static void main(String[] args) {
         // printf() => a method used to format output
+        // doesn't end on newline, requires '\n' character for it
+        // %[flag][width][.precision][specifier-character]
+
+        String name = "Chaman"; // %s for strings 
+        char firstLetter = 'C'; // %c for characters
+        int age = 10; // %d for integers
+        double height = 60.6; // and for float and double we use %f or %.f and number of digits for decimal places
+        boolean isStudent = true;
+
+        System.out.printf("Hello %s, Your name starts with %c\nYour age is %d years and height is %.1f\nAre you a student? %b", name, firstLetter, age, height, isStudent); // variable ka naam same order me dena hai jaise use aaye
     }
 }
