ee2167beb8421bc29189f9e6bd0037a55b055f91
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-08 17:58:09 +0530

    Add formatted output for prices in printfStatment class with precision examples

--- BroCode_Java/printfStatment.java
+++ BroCode_Java/printfStatment.java
@@ -11,5 +11,12 @@ public class printfStatment {
         boolean isStudent = true;
 
         System.out.printf("Hello %s, Your name starts with %c\nYour age is %d years and height is %.1f\nAre you a student? %b", name, firstLetter, age, height, isStudent); // variable ka naam same order me dena hai jaise use aaye
+
+        double price1 = 9.99;
+        double price2 = 19.99;
+        double price3 = 29.99;
+
+        System.out.printf("\n%f\n%f\n%f", price1, price2, price3); // without precision specified, the numbers will take 6 places after decimal
+        System.out.printf("\n%.2f\n%.2f\n%.2f", price1, price2, price3); // here specified the precision that after decimal we want only two decimal places
     }
 }
