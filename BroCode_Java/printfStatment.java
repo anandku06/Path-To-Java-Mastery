1f86bc57e4a798a6f3d8c99e21c1d4dff730ef85
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-08 18:07:32 +0530

    Add additional formatted output examples in printfStatment class with flags and precision

--- BroCode_Java/printfStatment.java
+++ BroCode_Java/printfStatment.java
@@ -13,10 +13,25 @@ public class printfStatment {
         System.out.printf("Hello %s, Your name starts with %c\nYour age is %d years and height is %.1f\nAre you a student? %b", name, firstLetter, age, height, isStudent); // variable ka naam same order me dena hai jaise use aaye
 
         double price1 = 9.99;
-        double price2 = 19.99;
-        double price3 = 29.99;
+        double price2 = 19000000.99;
+        double price3 = -29.99;
 
         System.out.printf("\n%f\n%f\n%f", price1, price2, price3); // without precision specified, the numbers will take 6 places after decimal
         System.out.printf("\n%.2f\n%.2f\n%.2f", price1, price2, price3); // here specified the precision that after decimal we want only two decimal places
+
+        // [flags]
+
+        /* 
+         * + => output a plus
+         * , => comma grouping seperator
+         * ( => negative numbers are enclosed in ()
+         * 'space' => display a minus if negative, space if positive
+         */
+
+         System.out.printf("\n%+.2f", price1); // displayed '+' sign before the number
+         System.out.printf("\n%,.2f", price2); // seperates the digits in thousands order
+         System.out.printf("\n%(.2f", price3); // negative number is enclosed in braces
+         System.out.printf("\n% .2f", price3); // space if positive, else the negative sign is there
+
     }
 }
