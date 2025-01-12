89fa1aaa183803df8497d90f5f9280555a74f5b2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 15:09:15 +0530

    Add user input for student discount in NestedIf class

--- BroCode_Java/NestedIf.java
+++ BroCode_Java/NestedIf.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /* Nested If -> if k andar if 
  * if () {
         if(){}
@@ -6,15 +9,21 @@
 
 public class NestedIf {
     public static void main(String[] args) {
-        boolean isStudent = true, isSenior;
+        boolean isStudent, isSenior;
         double price = 9.99;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Are you a student? (true/false) -> ");
+        isStudent = scanner.nextBoolean();
+
         if(isStudent){
-            System.out.println("You'll get a student discount!!");
+            System.out.println("You'll get a student discount of 10%!!");
             price *= 0.9;
+            System.out.printf("Now the price you have to pay is -> %,.2f", price);
         }
         else{
-            System.out.println("You'll not get student discount :(");
+            System.out.println("You won't get any student discount :(");
         }
     }
 }
