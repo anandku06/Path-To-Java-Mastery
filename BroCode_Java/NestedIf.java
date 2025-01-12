c5b5eeb84b28273c31b520125563dc6a7ee1cd64
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 15:04:33 +0530

    Implement student discount logic in NestedIf class

--- BroCode_Java/NestedIf.java
+++ BroCode_Java/NestedIf.java
@@ -1,5 +1,20 @@
+/* Nested If -> if k andar if 
+ * if () {
+        if(){}
+    }
+*/
+
 public class NestedIf {
     public static void main(String[] args) {
-        
+        boolean isStudent = true, isSenior;
+        double price = 9.99;
+
+        if(isStudent){
+            System.out.println("You'll get a student discount!!");
+            price *= 0.9;
+        }
+        else{
+            System.out.println("You'll not get student discount :(");
+        }
     }
 }
