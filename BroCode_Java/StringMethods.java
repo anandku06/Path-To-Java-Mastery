fff54d8ca18be6f96a30c6b0869000d087ebc6f2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 19:22:29 +0530

    Add examples of string methods in StringMethods class

--- BroCode_Java/StringMethods.java
+++ BroCode_Java/StringMethods.java
@@ -1,5 +1,17 @@
+/*
+ * String is a class that has some methods which can be accessed using dot notation and gives some useful information regarding the data
+ */
+
 public class StringMethods {
     public static void main(String[] args) {
-        
+        String name = "Anand";
+
+        int size = name.length(); // returns the length of the string
+        char fLetter = name.charAt(0); // returns the character on the specified index
+
+
+
+
+        System.out.println(fLetter);
     }
 }
