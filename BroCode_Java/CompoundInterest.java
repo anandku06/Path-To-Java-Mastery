6a2c7024763a396ce19968a82c59d613bf79a2bb
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 14:28:03 +0530

    Add main method to CompoundInterest class for user input handling

--- BroCode_Java/CompoundInterest.java
+++ BroCode_Java/CompoundInterest.java
@@ -1,3 +1,14 @@
+
+import java.util.Scanner;
+
 public class CompoundInterest {
-    
+    public static void main(){
+        Scanner scanner = new Scanner(System.in);
+        double principal, rate, amount;
+        int timeCompounded, years;
+
+
+        
+        scanner.close();
+    }
 }
