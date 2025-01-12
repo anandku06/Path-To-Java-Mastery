940974a108c270b8cd2a7145b36078bcb0889820
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 19:30:12 +0530

    Enhance StringMethods class with additional string manipulation examples

--- BroCode_Java/StringMethods.java
+++ BroCode_Java/StringMethods.java
@@ -4,14 +4,16 @@
 
 public class StringMethods {
     public static void main(String[] args) {
-        String name = "Anand";
+        String name = "    Anand   ";
 
         int size = name.length(); // returns the length of the string
         char fLetter = name.charAt(0); // returns the character on the specified index
+        int index = name.indexOf("d"); // returns the first occurence of the character specified
+        int lIndex = name.lastIndexOf('n'); // returns the last index of the specified character
+        name = name.toUpperCase(); // changes the string to uppercase
+        name = name.toLowerCase(); // changes to lowercase
+        name = name.trim(); // trims off or removes all whitespaces from the string
 
-
-
-
-        System.out.println(fLetter);
+        System.out.println(name);
     }
 }
