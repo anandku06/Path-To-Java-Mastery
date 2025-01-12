cee738b520a19efded5ec3baa89ac0736258e10c
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-12 14:47:47 +0530

    Add user input prompts and calculation for compound interest in CompoundInterest class

--- BroCode_Java/CompoundInterest.java
+++ BroCode_Java/CompoundInterest.java
@@ -6,9 +6,23 @@ public class CompoundInterest {
         Scanner scanner = new Scanner(System.in);
         double principal, rate, amount;
         int timeCompounded, years;
-
-
         
+        System.out.print("Enter the principal amount -> ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the rate of interest (in %) -> ");
+        rate = scanner.nextDouble() / 100.0;
+
+        System.out.print("Enter the number of times compounded per year -> ");
+        timeCompounded = scanner.nextInt();
+
+        System.out.print("Enter the years -> ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + (rate / timeCompounded), (timeCompounded * years));
+
+        System.out.printf("The amount calculated would be -> %,.2f", amount);
+
         scanner.close();
     }
 }
