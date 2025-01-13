954b773970b50480907bf1edd31148bf49849708
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 00:11:39 +0530

    Add substring examples in Substring class main method

--- BroCode_Java/Substring.java
+++ BroCode_Java/Substring.java
@@ -1,5 +1,15 @@
+/* .substring() -> a method used to extract a portion of string.
+ * syntax: .substring(start, end); start point of the string and end point of the string as args(exclusive)
+*/
+
 public class Substring {
     public static void main(String[] args) {
-        
+        String email = "keshujichusenge@loda.arc";
+        String userName = email.substring(0, 15);
+        // String domain = email.substring(16, 24);
+        String domain = email.substring(16); // upto the ending index
+
+        System.out.println(userName);
+        System.out.println(domain);
     }
 }
