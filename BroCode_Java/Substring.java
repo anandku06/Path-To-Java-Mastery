48884d643628673ff50247e6b07cc9be6bff483d
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 00:17:41 +0530

    Refactor Substring class to accept user input for email and extract username and domain dynamically

--- BroCode_Java/Substring.java
+++ BroCode_Java/Substring.java
@@ -2,14 +2,23 @@
  * syntax: .substring(start, end); start point of the string and end point of the string as args(exclusive)
 */
 
+import java.util.Scanner;
+
 public class Substring {
     public static void main(String[] args) {
-        String email = "keshujichusenge@loda.arc";
-        String userName = email.substring(0, 15);
+        Scanner sc = new Scanner(System.in);
+        String email;
+
+        System.out.print("Enter your email -> ");
+        email = sc.nextLine();
+        // String userName = email.substring(0, 15);
         // String domain = email.substring(16, 24);
-        String domain = email.substring(16); // upto the ending index
+        String userName = email.substring(0,email.indexOf('@'));
+        String domain = email.substring(email.indexOf('@') + 1); // using .indexOf() method to get the index of the '@' character
 
         System.out.println(userName);
         System.out.println(domain);
+
+        sc.close();
     }
 }
