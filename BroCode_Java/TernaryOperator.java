47fe201335ac69671c737c43e33d764dcb4cf934
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 18:46:03 +0530

    Add comments and example usage for ternary operator in TernaryOperator class

--- BroCode_Java/TernaryOperator.java
+++ BroCode_Java/TernaryOperator.java
@@ -1,5 +1,16 @@
 public class TernaryOperator {
     public static void main(String[] args) {
-        
+        // ternary operator ? => return 1 or 2 values if a condition is true
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        int score = 70;
+
+        //traditionally
+        if(score < 100){
+            System.out.println("Better luck next time!!!");
+        }
+        else{
+            System.out.println("Bravo!! more to go");
+        }
     }
 }
