b7e4478af75c275c313fdebb7fe554d49680768e
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 18:31:05 +0530

    Add user prompts for weight conversion options in WeightConversion class

--- BroCode_Java/WeightConversion.java
+++ BroCode_Java/WeightConversion.java
@@ -9,8 +9,16 @@ public class WeightConversion {
         int choice;
 
         // welcome message
-
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2: Convert kgs to lbs");
 
         // prompt for user choice
+        System.out.print("Choose your preference -> ");
+        choice = sc.nextInt();
+
+        if(choice == 1){
+            
+        }
     }
 }
