73db70b02c6334836bc542c3e4801b1b2e5b097b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 18:49:30 +0530

    Add user input for score and implement ternary operator evaluation in TernaryOperator class

--- BroCode_Java/TernaryOperator.java
+++ BroCode_Java/TernaryOperator.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class TernaryOperator {
     public static void main(String[] args) {
         // ternary operator ? => return 1 or 2 values if a condition is true
         // variable = (condition) ? ifTrue : ifFalse;
 
-        int score = 70;
+        int score;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your score -> ");
+        score = sc.nextInt();
 
         //traditionally
         if(score < 100){
@@ -12,5 +18,13 @@ public class TernaryOperator {
         else{
             System.out.println("Bravo!! more to go");
         }
+
+        // using ternary op
+
+        String passOrFail = (score < 100) ? "Failed! better luck next time" : "Passed!! more to go";
+
+        System.out.println(passOrFail);
+
+        sc.close();
     }
 }
