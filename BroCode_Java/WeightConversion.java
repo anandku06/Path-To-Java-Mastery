7a5913a5e898307e18917da65a81598f5d3ea9a1
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-14 18:28:06 +0530

    Add user input handling and variable declarations in WeightConversion class

--- BroCode_Java/WeightConversion.java
+++ BroCode_Java/WeightConversion.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class WeightConversion {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        // Declaring vars
+
+        double weight, newWeight;
+        int choice;
+
+        // welcome message
+
+
+        // prompt for user choice
     }
 }
