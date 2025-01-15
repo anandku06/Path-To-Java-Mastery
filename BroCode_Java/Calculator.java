310a2e36f684148ebf68bd73d781cbcdd3225ef6
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 17:09:05 +0530

    Fix prompt for second number input in Calculator class

--- BroCode_Java/Calculator.java
+++ BroCode_Java/Calculator.java
@@ -13,8 +13,8 @@ public class Calculator {
         System.out.print("Enter the operation (+, -, *, /, ^) -> ");
         operator = sc.next().charAt(0); // as the next() accepts only string so, method chaining the charAt() will return the char at the first index
 
-        System.out.print("Enter your first number -> ");
-        num1 = sc.nextDouble();
+        System.out.print("Enter your second number -> ");
+        num2 = sc.nextDouble();
 
         sc.close();
     }
