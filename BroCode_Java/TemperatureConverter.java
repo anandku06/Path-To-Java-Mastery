2f497d8909662271fa93b243b0c60259fbf57800
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 16:30:01 +0530

    Enhance temperature conversion functionality to include detailed unit prompts and output formatting

--- BroCode_Java/TemperatureConverter.java
+++ BroCode_Java/TemperatureConverter.java
@@ -10,9 +10,14 @@ public class TemperatureConverter {
         System.out.print("Enter the current temperature -> ");
         temp = sc.nextDouble();
 
-        System.out.print("Convert to? (C or F) -> ");
+        System.out.print("Convert to? (°C or °F) -> ");
         unit = sc.next().toUpperCase(); // also called method chaining ; when two methods are used together
-        
+
+        // (condition) ? ifTrue : ifFalse;
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+        System.out.printf("Your temperature in °%s is -> %.2f°%s", unit, newTemp, unit);
         sc.close();
     }
 }
