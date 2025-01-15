fc5a7804425013f1370d55ba5fb322e970d803b9
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 16:51:34 +0530

    Refactor EnhancedSwitch to use new switch expression syntax for improved readability

--- BroCode_Java/EnhancedSwitch.java
+++ BroCode_Java/EnhancedSwitch.java
@@ -13,32 +13,52 @@ public class EnhancedSwitch {
 
         // traditionally
 
-        switch (day) {
-            case "Monday":
-                System.out.println("It's a working day(┬┬﹏┬┬)");
-                break;
-            case "Tuesday":
-                System.out.println("It's a working day(┬┬﹏┬┬)");
-                break;
-            case "Wednesday":
-                System.out.println("It's a working day(┬┬﹏┬┬)");
-                break;
-            case "Thursday":
-                System.out.println("It's a working day(┬┬﹏┬┬)");
-                break;
-            case "Friday":
-                System.out.println("It's a last working day(‾◡◝)");
-                break;
-            case "Saturday":
-                System.out.println("It's a Holiday^_^");
-                break;
-            case "Sunday":
-                System.out.println("It's a Holiday^_^");
-                break;
+        // switch (day) {
+        //     case "Monday":
+        //         System.out.println("It's a working day(┬┬﹏┬┬)");
+        //         break;
+        //     case "Tuesday":
+        //         System.out.println("It's a working day(┬┬﹏┬┬)");
+        //         break;
+        //     case "Wednesday":
+        //         System.out.println("It's a working day(┬┬﹏┬┬)");
+        //         break;
+        //     case "Thursday":
+        //         System.out.println("It's a working day(┬┬﹏┬┬)");
+        //         break;
+        //     case "Friday":
+        //         System.out.println("It's a last working day(‾◡◝)");
+        //         break;
+        //     case "Saturday":
+        //         System.out.println("It's a Holiday^_^");
+        //         break;
+        //     case "Sunday":
+        //         System.out.println("It's a Holiday^_^");
+        //         break;
         
-            default: // if none of the cases matched, then execute this
-                System.out.printf("%s is not a day!!", day);
-                break;
+        //     default: // if none of the cases matched, then execute this
+        //         System.out.printf("%s is not a day!!", day);
+        //         break;
+        // }
+        
+        // new Syntax
+        
+        switch(day){
+            case "Monday" -> System.out.println("It's a working day(┬┬﹏┬┬)");
+
+            case "Tuesday" -> System.out.println("It's a working day(┬┬﹏┬┬)");
+
+            case "Wednesday" -> System.out.println("It's a working day(┬┬﹏┬┬)");
+
+            case "Thursday" -> System.out.println("It's a working day(┬┬﹏┬┬)");
+            
+            case "Friday" -> System.out.println("It's a working day(┬┬﹏┬┬)");
+
+            case "Saturday" ->System.out.println("It's a Holiday^_^");
+
+            case "Sunday" ->System.out.println("It's a Holiday^_^");
+
+            default -> System.out.printf("%s is not a day!!");
         }
 
         sc.close();
