b799ccae195645684b8c94d0e490b66d9937effc
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 17:23:09 +0530

    Enhance Calculator class to handle multiple operations and validate division by zero

--- BroCode_Java/Calculator.java
+++ BroCode_Java/Calculator.java
@@ -4,8 +4,9 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double num1, num2, result;
+        double num1, num2, result = 0.0;
         char operator;
+        boolean validOperation = true;
 
         System.out.print("Enter your first number -> ");
         num1 = sc.nextDouble();
@@ -15,6 +16,35 @@ public class Calculator {
 
         System.out.print("Enter your second number -> ");
         num2 = sc.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1 + num2;
+
+            case '*' -> result = num1 * num2;
+
+            case '-' -> result = num1 - num2;
+
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("Zero division not allowed!!");
+                    validOperation = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            } // if multiple lines then use braces
+
+            case '^' -> result = Math.pow(num1, num2);
+
+            default -> {
+                System.out.println("Enter a valid operation!!");
+                validOperation = false;
+            } 
+        }
+
+        if (validOperation) { // if the operation is valid then only print result
+            System.out.println(result);
+        }
 
         sc.close();
     }
