b7b0c5c45515f42a390d202d4d165eb41f94661b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 18:49:48 +0530

    Update weather condition checks in LogicalOperators class for temperature and sunny status

--- BroCode_Java/LogicalOperators.java
+++ BroCode_Java/LogicalOperators.java
@@ -5,14 +5,17 @@ public class LogicalOperators {
         // || -> OR -> when any one condition is true then true
         // ! -> NOT -> if true then false and vice-versa
 
-        double temp = 20;
-        boolean isSunny = true;
+        double temp = 35;
+        boolean isSunny = false;
 
         if(temp < 30 && temp >= 0 && isSunny){ 
             System.out.println("The weather is GOOD and its sunny outside!!!");
         } // here temp should be less than 30 AND greater than and equal to 0 AND isSunny should be true, then true
         else if(temp < 30 && temp >= 0 && !isSunny){
-            
+            System.out.println("Its windy outside but the weather is GOOD!!!");
         } // here the isSunny must be false, as the NOT op will turn the false to true, so the condition will be true
+        else if(temp > 30 || temp < 0){
+            System.out.println("The weather is bad!!");
+        } // here either of the condition must be true
     }
 }
