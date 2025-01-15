51b0f48a4569311da94279c878353a2f14555c4c
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 17:08:38 +0530

    Implement user input for calculator operations with basic prompts

--- BroCode_Java/Calculator.java
+++ BroCode_Java/Calculator.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter your first number -> ");
+        num1 = sc.nextDouble();
         
+        System.out.print("Enter the operation (+, -, *, /, ^) -> ");
+        operator = sc.next().charAt(0); // as the next() accepts only string so, method chaining the charAt() will return the char at the first index
+
+        System.out.print("Enter your first number -> ");
+        num1 = sc.nextDouble();
+
+        sc.close();
     }
 }
