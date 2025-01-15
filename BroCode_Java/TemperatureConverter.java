21676bba6fe333455909ec292ac08bf4f2838192
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 16:21:49 +0530

    Add user input functionality for temperature conversion in TemperatureConverter class

--- BroCode_Java/TemperatureConverter.java
+++ BroCode_Java/TemperatureConverter.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double temp, newTemp;
+        String unit;
+
+        System.out.print("Enter the current temperature -> ");
+        temp = sc.nextDouble();
+
+        System.out.print("Convert to? (C or F) -> ");
+        unit = sc.next().toUpperCase(); // also called method chaining ; when two methods are used together
         
+        sc.close();
     }
 }
