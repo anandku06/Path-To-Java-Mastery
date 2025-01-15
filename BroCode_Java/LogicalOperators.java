08d8834b4e17b025e2df69cbad4efda60664f1fa
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 18:42:31 +0530

    Add examples and comments for logical operators in LogicalOperators class

--- BroCode_Java/LogicalOperators.java
+++ BroCode_Java/LogicalOperators.java
@@ -1,5 +1,14 @@
 public class LogicalOperators {
     public static void main(String[] args) {
-        
+        // Logical Operators in Java used to check and modify more than one conditions simultaneously
+        // && -> AND -> when all the conditions are true then true else false
+        // || -> OR -> when any one condition is true then true
+        // ! -> NOT -> if true then false and vice-versa
+
+        double temp = 20;
+
+        if(temp < 30 && temp >= 0){ // here temp should be less than 30 AND greater than and equal to 0, then true
+            System.out.println("The weather is GOOD!!!");
+        }
     }
 }
