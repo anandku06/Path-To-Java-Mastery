8e0dec5ba04bc09e56e84728a8ffcd006aab03a9
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-15 19:06:03 +0530

    Implement while loop for user name input validation in WhileLoops class

--- BroCode_Java/WhileLoops.java
+++ BroCode_Java/WhileLoops.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
-        
+        // While loop -> repeat some code forever while the condition remains true
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = "";
+
+        if(name.isEmpty()){
+            System.out.print("Your name -> ");
+            name = sc.nextLine();
+        } // what if the user doesn't type a thing, then it'll print the blank
+
+        // to avoid this use while loop
+        while(name.isEmpty()){
+            System.out.print("Your name -> ");
+            name = sc.nextLine();
+        } // while the condition(name.isEmpty()) is true, run the enclosed code, untill it becomes false
+
+        System.out.printf("Hello %s", name);
+
+        sc.close();
     }
 }
