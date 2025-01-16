3ccd828fb828a0944ee0a2b225c4797f9a1265f2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-16 19:06:41 +0530

    Implement number guessing game logic in NumberGuessing class

--- BroCode_Java/NumberGuessing.java
+++ BroCode_Java/NumberGuessing.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuessing {
     public static void main(String[] args) {
-        
+        // Number guessing game
+
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int guess, attempts = 0, randomNumber;
+        randomNumber = rand.nextInt(11);
+
+        System.out.println("Number Guessing Game!!");
+        System.out.println("Guess a number between 1-10");
+
+        do{
+            System.out.print("Enter your guess -> ");
+            guess = sc.nextInt();
+            attempts++;
+        }while(guess != randomNumber);
+        System.out.println("You guessed it right!!");
+
+        sc.close();
     }
 }
