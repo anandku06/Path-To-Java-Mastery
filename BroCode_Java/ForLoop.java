4d47ffd13e8d0d5f1d027ef8955a2999d8900ca7
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-16 19:22:00 +0530

    Add for loop example and explanation in ForLoop class

--- BroCode_Java/ForLoop.java
+++ BroCode_Java/ForLoop.java
@@ -1,5 +1,16 @@
 public class ForLoop {
     public static void main(String[] args) {
-        
+        // for loop -> execute some code a certain amount of times
+        // syntax:
+        // for(initialisation; condition; updation){
+        //     // code
+        // }
+        // initialisation -> a counter that tracks the number of iterations
+        // condition -> a termination condition which met, then the loop ends
+        // updation -> counter increases or decreases accordingly
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Hello World!!");
+        }
     }
 }
