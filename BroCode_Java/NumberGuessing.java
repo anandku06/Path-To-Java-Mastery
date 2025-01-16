b6edca3b242d0121c56bd7918a53ef5a9559444d
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-16 19:11:34 +0530

    Refine number guessing game logic to include feedback for low and high guesses

--- BroCode_Java/NumberGuessing.java
+++ BroCode_Java/NumberGuessing.java
@@ -9,7 +9,7 @@ public class NumberGuessing {
         Scanner sc = new Scanner(System.in);
 
         int guess, attempts = 0, randomNumber;
-        randomNumber = rand.nextInt(11);
+        randomNumber = rand.nextInt(1,11);
 
         System.out.println("Number Guessing Game!!");
         System.out.println("Guess a number between 1-10");
@@ -18,8 +18,19 @@ public class NumberGuessing {
             System.out.print("Enter your guess -> ");
             guess = sc.nextInt();
             attempts++;
+
+            if(guess < randomNumber){
+                System.out.println("Too Low! try again!!");
+            }
+            else if(guess > randomNumber){
+                System.out.println("Too high!! Try again!");
+            }
+            else{
+                System.out.println("You guessed it right!!");
+                System.out.printf("In %d attempts", attempts);
+            }
+
         }while(guess != randomNumber);
-        System.out.println("You guessed it right!!");
 
         sc.close();
     }
