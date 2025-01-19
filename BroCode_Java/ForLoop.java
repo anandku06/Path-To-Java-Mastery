27153fcbc51a16cacbdfda1e188f7ae4111ecf20
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-19 16:40:47 +0530

    Enhance ForLoop class to accept user-defined termination value and add countdown feature

--- BroCode_Java/ForLoop.java
+++ BroCode_Java/ForLoop.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException { // if the sleep is interuptted then throws this error
         // for loop -> execute some code a certain amount of times
         // syntax:
         // for(initialisation; condition; updation){
@@ -9,8 +11,23 @@ public class ForLoop {
         // condition -> a termination condition which met, then the loop ends
         // updation -> counter increases or decreases accordingly
 
-        for(int i = 0; i < 10; i++){
-            System.out.println("Hello World!!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the termination -> ");
+        int term = sc.nextInt();
+        for(int i = 0; i < term; i++){
+            System.out.println(i);
         }
+
+        int start = 10;
+
+        for(int i = start; i >= 0 ; i--){
+            System.out.println(i);
+            Thread.sleep(1000); // sleeps the execution for the specified ms
+            // matlab har 1 sec me loop chlega
+        }
+
+        System.out.println("Happy New Year!!");
+        sc.close();
     }
 }
