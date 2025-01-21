8b74b15941c3af7b2ed2764d9c55c60097359ebc
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-21 18:51:30 +0530

    Add comments and example to BreakAndContinue class explaining break and continue statements

--- BroCode_Java/BreakAndContinue.java
+++ BroCode_Java/BreakAndContinue.java
@@ -1,5 +1,17 @@
 public class BreakAndContinue {
     public static void main(String[] args) {
-        
+        // break -> break out of a loop (STOPS)
+        // continue -> skip the current iteration (SKIP)
+
+        for(int i = 0; i < 10; i++){
+            if(i == 5)
+            break; // means agar i ki value 5 hogi to loop tutega
+            // shorthand for writing if-else when one statement is given
+
+            if (i == 2) {
+                continue; // skips krdega iteration jb i ki value 2 hogi
+            }
+            System.out.println(i + " ");
+        }
     }
 }
