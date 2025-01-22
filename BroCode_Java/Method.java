bc7aa6be5ffb7a9a7f797d05c9c7adf90cab838a
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-22 18:37:08 +0530

    Add user input for name and age in Method class and update happyBirthday method parameters

--- BroCode_Java/Method.java
+++ BroCode_Java/Method.java
@@ -1,22 +1,31 @@
+import java.util.Scanner;
+
 public class Method {
     public static void main(String[] args) {
         // method -> a block of reusable code that is executed when called () ; defined outside of the Main method
-        String name = "Anand";
-        int age = 25;
+        String name;
+        int age;
         // using these vars in the method, pass this as arguments
         // arguments are info we send from the Main method to other made methods ; given under the set of parenthesis
         // as without sending these info can't be accessed by the methods
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name -> ");
+        name = sc.nextLine();
 
+        System.out.print("Enter your age -> ");
+        age = sc.nextInt();
 
         happyBirthday(name, age); // arguments
         // any number of times this function can be called
     }
 
-    static void happyBirthday(String name, int age){ 
+    static void happyBirthday(String birthdayBoy, int age){ 
         // we set the original function to take some parameters
         // the number of the parameters must be same as the arguments given
+        System.out.println("");
         System.out.println("Happy Birthday to you!");
-        System.out.printf("Happy Birthday %s!\n!", name);
+        System.out.printf("Happy Birthday %s!\n!", birthdayBoy);
         System.out.printf("You are %d years old\n!", age);
         System.out.println("Happy Birthday to you!\n");
     } // void is the return type of this method (not returning anything)
