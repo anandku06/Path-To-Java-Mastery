e4dd1cf0a99e2ccdba22c02102e54be78f3b7406
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-22 17:56:30 +0530

    Add example and comments to NestedLoop class demonstrating nested loops

--- BroCode_Java/NestedLoop.java
+++ BroCode_Java/NestedLoop.java
@@ -1,5 +1,12 @@
 public class NestedLoop {
     public static void main(String[] args) {
-        
+        // nested loop -> a loop inside another loop ; used often with matrices and DSA
+
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        } // nested loop
     }
 }
