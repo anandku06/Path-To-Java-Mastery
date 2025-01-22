c9f53614cb92de220d573c6aa0bd87838001e991
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-22 18:01:51 +0530

    Add user input functionality to NestedLoop class for dynamic row, column, and symbol selection

--- BroCode_Java/NestedLoop.java
+++ BroCode_Java/NestedLoop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class NestedLoop {
     public static void main(String[] args) {
         // nested loop -> a loop inside another loop ; used often with matrices and DSA
@@ -8,5 +10,28 @@ public class NestedLoop {
             }
             System.out.println();
         } // nested loop
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows, columns;
+        char symbol;
+
+        System.out.print("Enter the number of rows -> ");
+        rows = sc.nextInt();
+
+        System.out.print("Enter the number of columns -> ");
+        columns = sc.nextInt();
+
+        System.out.print("Enter the symbol -> ");
+        symbol = sc.next().charAt(0);
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
