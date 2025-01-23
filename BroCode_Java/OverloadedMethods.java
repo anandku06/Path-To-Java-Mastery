ff1b52af781a898a032280aa39f5caf371ea1883
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-23 23:48:00 +0530

    Implement overloaded add methods with different parameter signatures in OverloadedMethods class

--- BroCode_Java/OverloadedMethods.java
+++ BroCode_Java/OverloadedMethods.java
@@ -1,5 +1,22 @@
 public class OverloadedMethods {
     public static void main(String[] args) {
-        
+        // Overloaded methods -> methods that share the same time,
+        //              but different parameters signature = name 
+        //              + parameters
+
+        System.out.println(add(12, 34));
+        System.out.println(add(123, 343, 435));
     }
+
+    static double add(double a, double b){
+        return a + b;
+    }
+
+    static int add(int a, int b){
+        return a + b;
+    } // can be of different datatype but having the same name
+
+    static double add(double a, double b, double c){
+        return a + b + c;
+    } // same name but have different number of parameters
 }
