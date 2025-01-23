f732c11b16a99c8847ab91815f2d22a4832b6c6d
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-23 23:52:47 +0530

    Add bakePizza method to create pizza from bread type in OverloadedMethods class

--- BroCode_Java/OverloadedMethods.java
+++ BroCode_Java/OverloadedMethods.java
@@ -6,6 +6,14 @@ public class OverloadedMethods {
 
         System.out.println(add(12, 34));
         System.out.println(add(123, 343, 435));
+
+        String pizza = bakePizza("flatbread");
+
+        System.out.println(pizza);
+    }
+
+    static String bakePizza(String bread){
+        return bread.toUpperCase() + " pizza";
     }
 
     static double add(double a, double b){
