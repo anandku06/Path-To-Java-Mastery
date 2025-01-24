0704eb248f79b7b4ebf7a84f67bbb6b3996c7a11
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-24 19:32:50 +0530

    Enhance Scope class with variable scope examples and method demonstration

--- BroCode_Java/Scope.java
+++ BroCode_Java/Scope.java
@@ -1,5 +1,22 @@
 public class Scope {
+
+    static int x = 3; // here this variable has what's called Class Scope ; consider it as a global variable that is accessed by any method if called
+
     public static void main(String[] args) {
+        // variable scope = where a variable can be accessed
+        int x = 1; // here the variable 'x' is in a local scope
+        // local scope is the scope in the method itself, it can't be accessed outside of that method
+        // here the class scoped var is replaced by the local scoped var
         
+        System.out.println(x);
+        
+        DoSomething();
+    }
+    
+    static void DoSomething(){
+        int x = 2; // here the scope of the variable is local for this method; no connection with the main method's 'x' var
+        // here the class scoped var is replaced by the local scoped var
+
+        System.out.println(x);
     }
 }
