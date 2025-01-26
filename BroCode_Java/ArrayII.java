6d38eac12d9dcd8f358a15f1da4db075592f78f7
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-26 19:40:39 +0530

    Implement dynamic array input in ArrayII class with user prompts and display functionality

--- BroCode_Java/ArrayII.java
+++ BroCode_Java/ArrayII.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ArrayII {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String[] foods = {}; // this is an empty array ; can't assigned any value after assigning it
         
+        System.out.print("Enter the size -> ");
+        int size = sc.nextInt();
+
+
+        String[] foods = new String[size]; // this is an empty array created dynamically using new keyword and values can be assigned in this array
+
+        sc.nextLine();
+        for(int i = 0; i < foods.length; i++){
+            System.out.print("Enter a food -> ");
+            foods[i] = sc.nextLine();
+        }
+        // looping through the empty array and on each iteration taking the input and storing it in the array
+
+        for(String food : foods){
+            System.out.println(food);
+        }
+
+        sc.close();
     }
 }
