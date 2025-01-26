eecdd0586238ed907b69d92896a5e6b9a1baa0b9
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-26 13:17:43 +0530

    Implement dice rolling functionality in DiceRoller class

--- BroCode_Java/DiceRoller.java
+++ BroCode_Java/DiceRoller.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class DiceRoller {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        Random rand = new Random();
+        int numOfDice, total;
+
+        System.out.print("Enter the # of dice to roll -> ");
+        numOfDice = sc.nextInt();
+
+        if(numOfDice > 0){
+            for(int i = 0; i < numOfDice; i++){
+                int roll = rand.nextInt(1, 7);
+                System.out.println("You rolled: " + roll);
+            }
+        }
+        else{
+            System.out.println("Number of dice must be positive!!");
+        }
+
+        sc.close();
     }
 }
