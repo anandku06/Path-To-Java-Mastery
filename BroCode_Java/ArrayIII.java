bc4b0930480944109286a659c08616709aead825
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-27 18:58:59 +0530

    Implement linear search functionality in ArrayIII class

--- BroCode_Java/ArrayIII.java
+++ BroCode_Java/ArrayIII.java
@@ -1,5 +1,22 @@
 public class ArrayIII {
     public static void main(String[] args) {
-        
+
+        // Linear Search
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int target = 2;
+        boolean isFound = false;
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] == target){
+                System.out.println("found it at index " + i + " !!");
+                isFound = true;
+                break;
+            }
+        }
+
+        if (!isFound) {
+            System.out.println("Element not found!!");
+        }
     }
 }
