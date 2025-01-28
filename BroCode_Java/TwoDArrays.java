8bc86e57bc5a47826c2302982034ddf24dee1392
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-28 17:10:25 +0530

    Refactor groceries initialization in TwoDArrays class for improved readability

--- BroCode_Java/TwoDArrays.java
+++ BroCode_Java/TwoDArrays.java
@@ -3,11 +3,11 @@ public class TwoDArrays {
         // 2D arrays -> an array where each element is an array
         //              useful for storing a matrix of data
 
-        String[] fruits = {"Apple", "Banana", "Cherry"};
-        String[] veggies = {"Tomato", "Potato", "Onion"};
-        String[] meats = {"chicken", "pork", "beef", "fish"};
-
-        String[][] groceries = {fruits, veggies, meats}; // this is an 2D array as each element is itself is an array
+        String[][] groceries = {
+            {"Apple", "Banana", "Cherry"}, 
+            {"Tomato", "Potato", "Onion"}, 
+            {"chicken", "pork", "beef", "fish"}
+        }; // this is an 2D array as each element is itself is an array
         // to access the elements of this array, we need nested loop
 
         for(String[] foods : groceries){
