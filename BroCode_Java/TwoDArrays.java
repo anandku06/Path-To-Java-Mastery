dc25addfe58cdfe42c6b126d475d9dabb8dce6e2
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-28 17:02:18 +0530

    Add example of 2D arrays in TwoDArrays class with nested loops for data access

--- BroCode_Java/TwoDArrays.java
+++ BroCode_Java/TwoDArrays.java
@@ -1,5 +1,19 @@
 public class TwoDArrays {
     public static void main(String[] args) {
-        
+        // 2D arrays -> an array where each element is an array
+        //              useful for storing a matrix of data
+
+        String[] fruits = {"Apple", "Banana", "Cherry"};
+        String[] veggies = {"Tomato", "Potato", "Onion"};
+        String[] meats = {"chicken", "pork", "beef", "fish"};
+
+        String[][] groceries = {fruits, veggies, meats}; // this is an 2D array as each element is itself is an array
+        // to access the elements of this array, we need nested loop
+
+        for(String[] foods : groceries){
+            for(String food : foods){
+                System.out.println(food);
+            }
+        } // first looping through the outer array and then each element's array by the inner loop
     }
 }
