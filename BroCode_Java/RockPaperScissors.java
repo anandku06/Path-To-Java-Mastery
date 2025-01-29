0c3f25c619627238974afe7a6bbc3ff9abdc3d2a
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-29 18:33:41 +0530

    Implement basic RockPaperScissors game logic with user input and validation

--- BroCode_Java/RockPaperScissors.java
+++ BroCode_Java/RockPaperScissors.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperScissors {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        String[] choices = {
+            "rock",
+            "paper",
+            "scissors"
+        };
+        String playerChoice, compChoice, playAgain = "yes";
+
+        System.out.print("Choose!! (rock, paper, scissors)");
+        playerChoice = sc.nextLine().toLowerCase();
+
+        if(!playerChoice.equals("rock") && !playerChoice.equals("scissors") && !playerChoice.equals("paper")){
+            System.out.println("Invalid choice!!");
+        }
+
+        compChoice = choices[rand.nextInt(0,3)];
+
+        sc.close();
     }
 }
