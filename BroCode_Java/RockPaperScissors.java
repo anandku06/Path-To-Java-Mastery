92726aec25f34b3fc40a948fb837118409982757
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-29 19:16:10 +0530

    Refactor RockPaperScissors game to include replay functionality and improve user interaction

--- BroCode_Java/RockPaperScissors.java
+++ BroCode_Java/RockPaperScissors.java
@@ -14,33 +14,36 @@ public class RockPaperScissors {
         };
         String playerChoice, compChoice, playAgain = "yes";
 
-        System.out.print("Choose!! (rock, paper, scissors) -> ");
-        playerChoice = sc.nextLine().toLowerCase();
+        do{
+            System.out.print("Choose!! (rock, paper, scissors) -> ");
+            playerChoice = sc.nextLine().toLowerCase();
+    
+            if(!playerChoice.equals("rock") && !playerChoice.equals("scissors") && !playerChoice.equals("paper")){
+                System.out.println("Invalid choice!!");
+                continue;
+            }
+    
+            compChoice = choices[rand.nextInt(0,3)];
+            System.out.println("Computer choice: " + compChoice);
+    
+            if(playerChoice.equals(compChoice)){
+                System.out.println("It's a tie!!");
+            }
+    
+            else if((playerChoice.equals("rock") && compChoice.equals("scissors")) || (playerChoice.equals("paper") && compChoice.equals("rock")) || (playerChoice.equals("scissors") && compChoice.equals("paper"))){
+                System.out.println("You Win!!!");
+            }
+    
+            else{
+                System.out.println("You Lose!!");
+            }
+    
+            System.out.print("Play again? (yes/no) -> ");
+            playAgain = sc.nextLine().toLowerCase();
 
-        if(!playerChoice.equals("rock") && !playerChoice.equals("scissors") && !playerChoice.equals("paper")){
-            System.out.println("Invalid choice!!");
-        }
+        }while(playAgain.equals("yes"));
 
-        compChoice = choices[rand.nextInt(0,3)];
-        System.out.println(compChoice);
-
-        if(playerChoice.equals(compChoice)){
-            System.out.println("It's a tie!!");
-        }
-
-        else if(playerChoice.equals("rock") && compChoice.equals("scissors")){
-            System.out.println("You Win!!!");
-        }
-        else if(playerChoice.equals("paper") && compChoice.equals("rock")){
-            System.out.println("You Win!!!");
-        }
-        else if(playerChoice.equals("scissors") && compChoice.equals("paper")){
-            System.out.println("You Win!!!");
-        }
-
-        else{
-            System.out.println("You Lose!!");
-        }
+        System.out.println("Thanks for Playing!!!");
 
         sc.close();
     }
