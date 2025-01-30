1c7c7acfb2012fe89d21eb0fa170176b218053fa
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-30 18:49:57 +0530

    Implement basic SlotMachine game with betting and balance management

--- BroCode_Java/SlotMachine.java
+++ BroCode_Java/SlotMachine.java
@@ -1,5 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class SlotMachine {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int balance = 100, bet, payout;
+        String[] row;
+
+        System.out.println("!! Welcome to Java Slots !!");
+        System.out.println("Symbols : 🥚 🎈 🎁 🎉 ☘️");
+
+        while (balance > 0) {
+            System.out.println("Current balance -> $" + balance);
+            System.out.print("Enter your bet amount -> $");
+            bet = sc.nextInt();
+
+            if(bet > balance){
+                System.out.println("Insufficient balance!!");
+                continue;
+            }
+            else if (bet <= 0){
+                System.out.println("Bets must be greater than 0(zero)!!");
+                continue;
+            }
+            else{
+                balance -= bet;
+            }
+
+            spinRow();
+        }
+
+        sc.close();
+    }
+
+    static String[] spinRow(){
+
+        String[] symbols = {"🥚", "🎈", "🎁", "🎉", "☘️"};
+        String[] row = new String[3];
+        Random rand = new Random();
+
         
+
+        return new String[0];
     }
 }
