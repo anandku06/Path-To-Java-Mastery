80013b8330f76acc10ea0e25785837f16451db47
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-31 20:06:14 +0530

    Refactor Car class to include instance methods for starting, stopping, driving, and braking; update Objects class to demonstrate object instantiation and method calls.

--- BroCode_Java/Car.java
+++ BroCode_Java/Car.java
@@ -1,10 +1,29 @@
 public class Car {
-    public static void main(String[] args) {
-        // Class attributes -> properties of the class
-        String make = "Ford";
-        String model = "Mustang";
-        int year = 2000;
-        double price = 59999.99;
-        boolean isRunning = true;
+    // Class attributes -> properties of the class
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2000;
+    double price = 59999.99;
+    boolean isRunning = true;
+
+    // here not using static keyword because we are instantiating the class ; we are making the objects of the class
+
+    // class methods
+    void start(){
+        isRunning = true;
+        System.out.println("You started the engine!!");
+    }
+
+    void stop(){
+        isRunning = false;
+        System.out.println("You stopped the engine!!");
+    }
+
+    void drive(){
+        System.out.println("You drive the " + this.model + "!!");
+    }
+
+    void brake(){
+        System.out.println("You brake the " + this.model + "!!");
     }
 }
--- BroCode_Java/Objects.java
+++ BroCode_Java/Objects.java
@@ -4,5 +4,22 @@ public class Objects {
         //              a reference datatype
         // a Class can behave as a blue-print for creating objects
 
+        // to make an object or an instance of a class (here Car)
+        Car car = new Car(); // just like we do for Scanner ; using 'new' keyword make an instance of the class
+
+        System.out.println(car); // this will give a memory address of that 'car' object
+
+        System.out.println(car.make); // to access the attributes/properties of the class use dot notations(.)
+        System.out.println(car.model);
+
+        System.out.println(car.isRunning);
+        car.isRunning = false; // the properties can be modified as well
+        System.out.println(car.isRunning);
+
+        car.start(); // methods declared inside the class can also be accessed by dot notation.
+        car.stop();
+
+        car.drive();
+        car.brake();
     }
 }
