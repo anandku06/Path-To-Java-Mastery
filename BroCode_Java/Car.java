aa138642f4f459c5f6a57c344fc3b6bb6a5e56d0
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-31 19:38:01 +0530

    Add Objects class with main method and initial comments; update Car class attributes

--- BroCode_Java/Car.java
+++ BroCode_Java/Car.java
@@ -1,9 +1,10 @@
 public class Car {
     public static void main(String[] args) {
-        // Objects -> an entity that holds data (attributes) and can perform actions
-        //              a reference datatype
-        // a Class can behave as a blue-print for creating objects
-
-        
+        // Class attributes -> properties of the class
+        String make = "Ford";
+        String model = "Mustang";
+        int year = 2000;
+        double price = 59999.99;
+        boolean isRunning = true;
     }
 }
---
+++ BroCode_Java/Objects.java
@@ -0,0 +1,8 @@
+public class Objects {
+    public static void main(String[] args) {
+        // Objects -> an entity that holds data (attributes) and can perform actions
+        //              a reference datatype
+        // a Class can behave as a blue-print for creating objects
+
+    }
+}
