9fb7d9f68e45252f0de26bd4906708965b9ce9cf
Commiter <161126335+anandku06@users.noreply.github.com>
2025-01-31 19:33:35 +0530

    Remove Objects class and add Car class with main method and comments

---
+++ BroCode_Java/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    public static void main(String[] args) {
+        // Objects -> an entity that holds data (attributes) and can perform actions
+        //              a reference datatype
+        // a Class can behave as a blue-print for creating objects
+
+        
+    }
+}
--- BroCode_Java/Objects.java
+++
@@ -1,5 +0,0 @@
-public class Objects {
-    public static void main(String[] args) {
-        
-    }
-}
