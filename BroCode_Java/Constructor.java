2d1f1c497620df13679c4666f262ae96939706d7
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-02 00:03:00 +0530

    Add Student class with constructor and update Constructor class to demonstrate object instantiation

--- BroCode_Java/Constructor.java
+++ BroCode_Java/Constructor.java
@@ -1,5 +1,17 @@
 public class Constructor {
     public static void main(String[] args) {
-        
+        // constructor -> A special methods to initialise objects
+        //                  you can pass arguments to a constructor and set up initial values
+
+        Student stud1 = new Student("Ayush", 21, 3.2); // here bcz the constructor is defined to take args so we given the data that can be used by this object
+        Student stud2 = new Student("Keshav", 22, 3.5);
+
+        System.out.println(stud1.name);
+        System.out.println(stud1.age);
+        System.out.println(stud1.gpa);
+
+        System.out.println(stud2.name);
+        System.out.println(stud2.age);
+        System.out.println(stud2.gpa);
     }
 }
---
+++ BroCode_Java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    // class attributes
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    // constructors are called whenever a object is made from the class
+    // first the name of the class and then a set of parenthesis that can be used to give parameters in future if needed
+    Student(String name, int age, double gpa){
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        // here using 'this' keyword we assigned the variables that can be given when making objects of this class
+        // here 'this' keyword is replaced by the new object we made with 'new' keyword
+    }
+}
