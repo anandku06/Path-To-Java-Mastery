22ed9556459b0adc9646e9fa580254cbf52a6244
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-02 16:05:15 +0530

    Add User class with constructors for username and email; update OverloadedConstructors to demonstrate object creation

--- BroCode_Java/OverloadedContructors.java
+++ BroCode_Java/OverloadedContructors.java
@@ -2,6 +2,11 @@ public class OverloadedContructors{
     public static void main(String[] args) {
         // overloaded contructors -> allow a class to have multiple constuctors, with different parameters lists, Enable objects to be initialised in various ways.
 
-        
+        // making a object of class User
+
+        User user1 = new User("ayyush08"); // given params bcz the constructor of User class is called
+
+        System.out.println(user1.userName);
+        System.out.println(user1.email);
     }
 }
--- BroCode_Java/User.java
+++ BroCode_Java/User.java
@@ -1,5 +1,21 @@
 public class User {
-    public static void main(String[] args) {
-        
+    // class attributes
+
+    String userName, email;
+    int age;
+
+    // initialising a constructor that sets the username of each User class's object made
+    User(String username){
+        this.userName = username;
+        this.email = "Not Provided";
+        this.age = 0;
     }
+
+    // made another constructor that sets username and email for the newly made object
+    User(String username, String email){
+        this.userName = username;
+        this.email = email;
+        this.age = 0;
+    }
+
 }
