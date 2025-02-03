be914e1da54986b273faab04b7b4d97a2a62b89c
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-03 18:10:45 +0530

    Add static attribute to Friend class to track total number of friends; update Static class to demonstrate shared attribute usage

--- BroCode_Java/Friend.java
+++ BroCode_Java/Friend.java
@@ -2,9 +2,11 @@ public class Friend {
     
     // data attributes
     String name;
+    static int numOfFriends; // by making this static this attributes will be shared across all the objects made rather than each having thier own copy
 
     Friend(String name){
         this.name = name;
+        numOfFriends++;
     }
 
 }
--- BroCode_Java/Static.java
+++ BroCode_Java/Static.java
@@ -4,6 +4,17 @@ public class Static {
         //              rather than to any specific object
         //  commonly used for utility methods or shared resources
 
-        
+       Friend frnd1 = new Friend("Ayush");
+       Friend frnd2 = new Friend("Keshuji");
+       
+       System.out.println(frnd1.name);
+       System.out.println(frnd1.numOfFriends);
+       // bcz each object has its own copy of attributes, so in place of 2 it displays 1
+
+       System.out.println(frnd2.numOfFriends);
+       // after making this attribute static, now it'll count all the occurences and is shared among all the objects made
+       // as this static attributes are of class, so it should be accessed by the class not the objects
+
+       System.out.println(Friend.numOfFriends); // this is the correct way to access static members of a class
     }
 }
