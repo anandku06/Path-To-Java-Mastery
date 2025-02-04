86bb21d201ff7808c3bb743b46ef4ffccf177e51
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-04 23:34:30 +0530

    Override move method in Fish class to implement swimming behavior; add comments for clarity on method overriding.

--- BroCode_Java/Fish.java
+++ BroCode_Java/Fish.java
@@ -1,6 +1,8 @@
 public class Fish extends Animal{
     
     @Override // using this to signify that this method is overridden
+    // also provides a check that this method can't be spelled wrong
+    // if it happens then it throws an error, telling that this method is not being overridden
     void move(){
         System.out.println("This animal is swimming!!");
     }
