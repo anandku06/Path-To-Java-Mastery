09c4aaf9e3d80599800a0b31e85bc1f63a155294
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-05 17:39:15 +0530

    Add eat method to Cat and Dog classes; introduce isAlive property for both classes

--- BroCode_Java/Cat.java
+++ BroCode_Java/Cat.java
@@ -2,9 +2,13 @@ public class Cat extends Animal{
     // now the Cat class has all the properties of Animal class
 
     int lives = 9;
+    boolean isAlive = true;
 
     void speak(){
         System.out.println("The cat goes *meow*!!");
     }
 
+    void eat(){
+        System.out.println("This animal is eating!!");
+    }
 }
--- BroCode_Java/Dog.java
+++ BroCode_Java/Dog.java
@@ -2,8 +2,13 @@ public class Dog extends Animal{
     // now the Dog class has all the properties of Animal class
 
     int lives = 1;
+    boolean isAlive = true;
 
     void speak(){
         System.out.println("The dog goes *woof*!!");
+    }
+
+    void eat(){
+        System.out.println("This animal is eating!!");
     }
 }
