45ebd32478565dec8bc9cce9f07863eea6c9292a
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-06 20:20:59 +0530

    Add Interface class to demonstrate the concept of interfaces in Java

---
+++ BroCode_Java/Interface.java
@@ -0,0 +1,8 @@
+public class Interface {
+    public static void main(String[] args) {
+        // interface -> a blueprint for a class that specifies a set of abstract methods that implementing classes MUST define
+        // Supports multiple inheritance-like behaviour
+
+        
+    }
+}
