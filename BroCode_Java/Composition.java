1d721732005c086f7dd8d53bcbad68c62fdfc823
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-12 00:27:33 +0530

    Add Composition and Engine classes to illustrate "part-of" relationship in Java

---
+++ BroCode_Java/Composition.java
@@ -0,0 +1,9 @@
+public class Composition {
+    public static void main(String[] args) {
+        // Composition -> represents a "part-of" relationship between objects
+        // for ex : an Engine is "part of" a Car
+        // allows complex objects to be constructed from smaller objects
+
+        
+    }
+}
