29e13c4626b8e6425a0e4053ff4c75d91568fefd
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-12 01:09:24 +0530

    Enhance Wrapper class to demonstrate autoboxing, unboxing, and string conversion methods for primitive types

--- BroCode_Java/Wrapper.java
+++ BroCode_Java/Wrapper.java
@@ -1,3 +1,5 @@
+import javax.swing.text.html.HTMLDocument.BlockElement;
+
 public class Wrapper {
     public static void main(String[] args) {
         
@@ -6,12 +8,38 @@ public class Wrapper {
         // Generally, don't wrap primitives unless you need an object
         // allow use of Collection Framework and static Utility methods
 
-        int a = 123; 
-        // It is deprecated 
         // here when using Wrapper class, it looks like this -> Integer a = new Integer(123); -> here a is now a Integer object
-        // same goes for other values like
-        double d = 123.45; // Double d = new Double(123.45); 
-        char c = '$'; // Char c = new Char('$');
-        boolean bool = true; // Boolean bool = new Boolean(true);
+        // It is deprecated 
+        // same goes for other values like Boolean, Double, Character
+
+        // Autoboxing -> directly assigning the values to the objects without using the new keyword
+        Integer a = 123; 
+        Double d = 123.45; 
+        Character c = '$';
+        Boolean bool = true;
+        String name = "Anand";
+
+        // Unboxing ->  taking the boxed object and assigning it to its primitive value
+        int x = a; // now the Integer object 'a' is unboxed and its value stored in x
+
+        // converting other datatypes to String by using the .toString() method
+        String i = Integer.toString(123);
+        String j = Double.toString(123.45);
+        String k = Character.toString('#');
+        String l = Boolean.toString(true);
+
+        String q = i + j + k + l; // string concatenation
+
+        System.out.println(q);
+
+        // converting String to other datatypes
+        a = Integer.parseInt("354"); // using the .parseInt() method to convert the string to int
+        d = Double.parseDouble("3.14");
+        bool = Boolean.parseBoolean("true");
+
+        c = "Pizza".charAt(0); // using the indexAt() method for char aas they haven't got any parse methods
+
+        System.out.println(Character.isLetter(c)); // returns whether the specified element is a character or not
+        System.out.println(Character.isUpperCase(c)); // returns whether the specified element is in uppercase or not
     }
 }
