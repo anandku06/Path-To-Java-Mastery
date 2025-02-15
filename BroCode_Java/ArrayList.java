364c5ed360840052a89397d6f82eed76221fee07
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-15 19:56:25 +0530

    Add elements to the list and displaying it

--- BroCode_Java/ArrayList.java
+++ BroCode_Java/ArrayList.java
@@ -1,6 +1,18 @@
-public class Main{
+import java.util.ArrayList;
+
+public class Lists{
     public static void main(String[] args){
         // ArrayList -> A resizable array that stores objects (autoboxing)
         // Arrays are fixed in size, but ArrayLists can change
+
+        // syntax: ArrayList<WrapperClass> nameOfList = new ArrayList<>();
+        // <> is diamond operator
+        ArrayList<Integer> list  = new ArrayList<>(); // makes an arraylist of intergers
+        list.add(45); // add method used to add the elements to the list
+        list.add(12);
+        list.add(23);
+        list.add(456);
+
+        System.out.println(list);
     }
 }
