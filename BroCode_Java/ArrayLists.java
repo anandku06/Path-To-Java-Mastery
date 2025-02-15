f83865a7403109cfd5a5f7c502bbc25c0a5cd579
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-15 20:07:44 +0530

    Refactor ArrayList example: replace Lists class with ArrayLists class and improve code structure

--- BroCode_Java/ArrayLists.java
+++ BroCode_Java/ArrayLists.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Lists{
+public class ArrayLists{
     public static void main(String[] args){
         // ArrayList -> A resizable array that stores objects (autoboxing)
         // Arrays are fixed in size, but ArrayLists can change
