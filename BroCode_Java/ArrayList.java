2d0dbce34e0ceb22afff562a5aca9c1afba293e5
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-15 19:40:32 +0530

    Learn ArrayLists in Java

---
+++ BroCode_Java/ArrayList.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        // ArrayList -> A resizable array that stores objects (autoboxing)
+        // Arrays are fixed in size, but ArrayLists can change
+    }
+}
