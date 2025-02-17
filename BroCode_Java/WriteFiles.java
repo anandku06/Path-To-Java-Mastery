d1fa51b8c663d13bf95f9f302d2ee1d3855bb095
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-18 03:08:15 +0530

    Enhance WriteFiles class to include file path variable, append content, and improve exception handling

--- BroCode_Java/WriteFiles.java
+++ BroCode_Java/WriteFiles.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,10 +11,19 @@ public class WriteFiles {
         // PrintWriter -> best for structured data, like reports or logs
         // FileOutputStream -> best for binary files (e.g. images, audio files)
 
+        String filePath = "test.txt"; // contains the new path for the file : for the filepath use double backslash
+        String textContent = "I like Pizza!\nIt's really nice!!"; // now the text content is in this variable
+        String textContent2 = "I also like Burger as well\n and I am huungry as well\n Please buy me Pizza or Burger!!";
+
         // FileWriter object used to create a file with the specified name -> as it can be fail, so enclosing it with try-resources...catch block to handle any errors
-        try(FileWriter writer = new FileWriter("test.txt")){
-            writer.write("I like Pizza"); // writes any string data to the file
+        // can also give the path, where the file should be created
+        try(FileWriter writer = new FileWriter(filePath)){
+            writer.write(textContent); // writes any string data to the file
+            writer.append(textContent2); // appends the text to the file(not overwritng the previous text)
             System.out.println("File has been written!!");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found!!");
         }
         catch(IOException e){
             System.out.println("File creation failed!!");
