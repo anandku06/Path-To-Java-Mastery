ffcd91b0f016c028f707089cd111bdce1e77d029
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-18 21:24:36 +0530

    Add ReadFile class to demonstrate file reading with BufferedReader and exception handling

--- BroCode_Java/ReadFile.java
+++ BroCode_Java/ReadFile.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadFile {
     public static void main(String[] args) {
         // how to read a file using Java
@@ -6,6 +11,17 @@ public class ReadFile {
         // FileInputStream -> best for binary files (e.g. images audio files)
         // RandomAccessFile: best for read/write specific portions of a large files
         
-        
+        String filePath = "C:\\Users\\risha\\OneDrive\\Desktop\\test.txt";
+        // path of the file to be read
+
+        // reading the writed file can be dangerous code, so enclosing it in try-catch block
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
     }
 }
