50795a045591d547eee1a0254eb028d30b9fed4f
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-18 22:16:43 +0530

    Add audio playback functionality to MusicPlayer class

--- BroCode_Java/MusicPlayer.java
+++ BroCode_Java/MusicPlayer.java
@@ -1,7 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream; // represents a stream of audio data that can be read and processed.
+
+import javax.sound.sampled.AudioSystem; // AudioSystem is a class that provides access to the system's audio resources.
+// getAudioInputStream(File file) is a static method that takes an audio file (like WAV, AIFF, AU, or MP3) as input.
+
+import javax.sound.sampled.UnsupportedAudioFileException; // exception when the file's format is not supported
+
 public class MusicPlayer {
     public static void main(String[] args) {
         // how to play audio files with Java (.wav, .au. aiff)
 
-        
+        String filePath = "sampleAudio.wav";
+
+        File file = new File(filePath);
+
+        try {
+            AudioInputStream audio = AudioSystem.getAudioInputStream(file); // used to get the audio from the File object
+        } catch (UnsupportedAudioFileException | IOException e) {
+           System.out.println(e);
+        }
     }
 }
