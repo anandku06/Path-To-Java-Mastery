742c67b25f813e7db634edb19e2d4cecb95bb298
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-18 22:25:56 +0530

    Enhance MusicPlayer class to support audio playback with Clip and improved exception handling

--- BroCode_Java/MusicPlayer.java
+++ BroCode_Java/MusicPlayer.java
@@ -6,6 +6,10 @@ import javax.sound.sampled.AudioInputStream; // represents a stream of audio dat
 import javax.sound.sampled.AudioSystem; // AudioSystem is a class that provides access to the system's audio resources.
 // getAudioInputStream(File file) is a static method that takes an audio file (like WAV, AIFF, AU, or MP3) as input.
 
+import javax.sound.sampled.Clip; // The Clip interface represents a special kind of data line whose audio data can be loaded prior to playback, instead of being streamed in real time.
+
+import javax.sound.sampled.LineUnavailableException; // A LineUnavailableException is an exception indicating that a line cannot be opened because it is unavailable
+
 import javax.sound.sampled.UnsupportedAudioFileException; // exception when the file's format is not supported
 
 public class MusicPlayer {
@@ -16,9 +20,16 @@ public class MusicPlayer {
 
         File file = new File(filePath);
 
-        try {
-            AudioInputStream audio = AudioSystem.getAudioInputStream(file); // used to get the audio from the File object
-        } catch (UnsupportedAudioFileException | IOException e) {
+        try(AudioInputStream audio = AudioSystem.getAudioInputStream(file)) {
+            // used to get the audio from the File object
+
+            Clip clip = AudioSystem.getClip(); // Obtains a clip that can be used for playing back an audio file or an audio stream
+
+            clip.open(audio); // load audio data from an AudioInputStream into a Clip so that it can be played.
+
+            System.out.println("No problems till!!");
+
+        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
            System.out.println(e);
         }
     }
