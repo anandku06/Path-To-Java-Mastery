a9ef40439da94fa421465d806df4e9ad02aa5337
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-20 19:55:21 +0530

    Initialize HangmanGame with basic game setup and word state management

--- BroCode_Java/HangmanGame.java
+++ BroCode_Java/HangmanGame.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HangmanGame {
     public static void main(String[] args) {
+        String word = "Pizza";
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            wordState.add('_');
+        }
+        System.out.println("Welcome to JavaHangMan!!");
         
+
+        sc.close();
     }
 }
