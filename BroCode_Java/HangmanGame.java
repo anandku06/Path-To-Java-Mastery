57e26182b40ce1af6eb8c71bd0426a4867c9330b
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-20 20:04:28 +0530

    Add hangman art representation based on wrong guesses in HangmanGame

--- BroCode_Java/HangmanGame.java
+++ BroCode_Java/HangmanGame.java
@@ -10,12 +10,52 @@ public class HangmanGame {
         ArrayList<Character> wordState = new ArrayList<>();
         int wrongGuesses = 0;
 
-        for(int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             wordState.add('_');
         }
         System.out.println("Welcome to JavaHangMan!!");
-        
 
         sc.close();
+    }
+
+    static String getHangamnArt(int wrongGuesses){
+        return switch(wrongGuesses){
+            case 0 -> """
+                    
+
+
+                    """;
+            case 1 -> """
+                      o
+
+
+                    """;
+            case 2 -> """
+                      o
+                      |  
+
+                    """;
+            case 3 -> """
+                      o
+                     /|  
+
+                    """;
+            case 4 -> """
+                      o
+                     /|\\  
+
+                    """;
+            case 5 -> """
+                      o
+                     /|\\  
+                     /   
+                    """;
+            case 6 -> """
+                      o
+                     /|\\  
+                     / \\   
+                    """;
+            default -> "";                   
+        }
     }
 }
