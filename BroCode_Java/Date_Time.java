9252723c6be7a583897b7c37b04b60549f423171
Commiter <161126335+anandku06@users.noreply.github.com>
2025-02-24 22:17:52 +0530

    Enhance Date_Time class to demonstrate current date, time, and UTC timestamp retrieval in Java

--- BroCode_Java/Date_Time.java
+++ BroCode_Java/Date_Time.java
@@ -1,6 +1,21 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Date_Time {
     public static void main(String[] args) {
         // how to work with Dates & Times using Java
         // (LocalDate, LocalTime, LocalDateTime, UTC timestamp)
+
+        LocalDate date = LocalDate.now(); // returns the current date in local format -> yyyy-mm-dd
+        LocalTime time = LocalTime.now(); // returns the current time in 24hrs format with the after seconds
+        System.out.println(LocalDateTime.now()); // returns the current time and date both 
+
+        System.out.println(date); 
+        System.out.println(time); 
+
+        Instant instant = Instant.now(); // An instantaneous point on the time-line ; in UTC format
+        System.out.println(instant);
     }
 }
