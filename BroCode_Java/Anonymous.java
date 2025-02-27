ceab4628bbd3ec62c64087426269ed849908d1f6
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-28 00:40:25 +0530

    Refactor Anonymous class usage and remove TalkingDog class to demonstrate anonymous class implementation in Java

--- BroCode_Java/Anonymous.java
+++ BroCode_Java/Anonymous.java
@@ -5,9 +5,15 @@ public class Anonymous {
         // often used for one time uses (TimerTask, Runnable, Callbacks)
 
         Dog dog = new Dog();
-        TalkingDog scooby = new TalkingDog(); // if any anomaly or a unique case occurs, rather than making a new class use anonymous classes
+        // TalkingDog scooby = new TalkingDog(); // if any anomaly or a unique case occurs, rather than making a new class use anonymous classes
+        Dog dog1 = new Dog(){
+            @Override
+            void speak(){
+                System.out.println("This dog is unique and talks ENGLISH!!");
+            }
+        }; // this is how you use anonymous class to specify any unique case or object; this is not reused
 
         dog.speak();
-        scooby.speak();
+        dog1.speak();
     }
 }
--- BroCode_Java/TalkingDog.java
+++
@@ -1,7 +0,0 @@
-public class TalkingDog extends Dog{
-    @Override
-
-    void speak(){
-        System.out.println("This dog talks and says EAT!!!");
-    }
-}
