a87f4059635f78e360216fb1b3c3a412c29b6147
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-28 00:55:04 +0530

    Add Timer class to schedule tasks and define TimerTask for task execution

---
+++ BroCode_Java/Timer.java
@@ -0,0 +1,9 @@
+public class Timer {
+    public static void main(String[] args) {
+        // Timer -> class that schedules tasks at specific times or periodically
+        // useful for: sending notifications, scheduled updates, repetitive actions
+        // TimerTask -> represents the task that will be executed by the Timer
+        // you will extend the TimerTask class to define your task
+        // create a subclass of TimerTask and @Override run()
+    }
+}
