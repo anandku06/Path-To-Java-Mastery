24c5590c31cc199f610af7a9f4bcbaa891760814
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-01 01:02:58 +0530

    Add Generics class to demonstrate the concept of generics in Java

---
+++ BroCode_Java/Generics.java
@@ -0,0 +1,7 @@
+public class Generics {
+    public static void main(String[] args) {
+        // generics -> a concept where you can write a class, interface or method that is compatible with the different datatype
+        // <T> type parameter (placeholder that gets replaced with a real type)
+        // <String> type argument (specifies the type)
+    }
+}
