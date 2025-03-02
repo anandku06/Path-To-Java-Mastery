cd3883336f1cf00b0c47263997e47f75c97607db
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-02 18:52:35 +0530

    Add Day enum with associated day numbers to enhance enumeration functionality

--- BroCode_Java/Day.java
+++ BroCode_Java/Day.java
@@ -1,3 +1,16 @@
+// made a enum class with the name of Day
+
 public enum Day {
-    
+    // constants are in uppercase and the value associated with the constants are given inside the parenthesis
+    SUNDAY(1), MONDAY(2), TUESDAY(3), WEDNESDAY(4), THURSDAY(5), FRIDAY(6), SATURDAY(7);
+
+    private final int dayNumber;
+    // constructor is necessary
+    Day(int dayNumber){
+        this.dayNumber = dayNumber;
+    }
+
+    public int getDayNumber(){
+        return this.dayNumber;
+    }
 }
