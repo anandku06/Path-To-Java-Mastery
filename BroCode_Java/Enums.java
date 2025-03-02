57b23efb8854032f5e1d6b0ce06974516158dfe1
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-02 18:47:53 +0530

    Add Day enum and Enums class to demonstrate the use of enumerations in Java

---
+++ BroCode_Java/Enums.java
@@ -0,0 +1,9 @@
+public class Enums {
+    public static void main(String[] args) {
+        // Enums -> (Enumerations) a special kind of class that represents a fixed set of constants
+        // they improve code readability and are easy to maintain
+        // more efficient with the switches when comparing Strings
+
+
+    }
+}
