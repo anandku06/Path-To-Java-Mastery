16c7d2a15a7354200d05a5312e0ebcee3a70594b
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-02 19:28:35 +0530

    Enhance Day enum and Enums class with user input handling and improved comments

--- BroCode_Java/Day.java
+++ BroCode_Java/Day.java
@@ -1,16 +1,19 @@
 // made a enum class with the name of Day
 
 public enum Day {
-    // constants are in uppercase and the value associated with the constants are given inside the parenthesis
+    // constants are in uppercase and the value associated with the constants are
+    // given inside the parenthesis
     SUNDAY(1), MONDAY(2), TUESDAY(3), WEDNESDAY(4), THURSDAY(5), FRIDAY(6), SATURDAY(7);
 
+    // the variable assigned to each constant
     private final int dayNumber;
+
     // constructor is necessary
-    Day(int dayNumber){
+    Day(int dayNumber) {
         this.dayNumber = dayNumber;
     }
 
-    public int getDayNumber(){
+    public int getDayNumber() {
         return this.dayNumber;
     }
 }
--- BroCode_Java/Enums.java
+++ BroCode_Java/Enums.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class Enums {
     public static void main(String[] args) {
-        // Enums -> (Enumerations) a special kind of class that represents a fixed set of constants
+        // Enums -> (Enumerations) a special kind of class that represents a fixed set
+        // of constants
         // they improve code readability and are easy to maintain
         // more efficient with the switches when comparing Strings
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a day -> ");
+        String res = sc.nextLine().toUpperCase();
 
-
+        try{
+            Day day = Day.valueOf(res); // Converts a string to the corresponding enum constant
+    
+            System.out.println(day);
+            System.out.println(day.getDayNumber());
+    
+            switch (day) {
+                case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> System.out.println("It's a weekday!!");
+    
+                case SUNDAY, SATURDAY -> System.out.println("It's a weekend!!");
+            }
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("Please enter a valid Day!!");
+        }
     }
 }
