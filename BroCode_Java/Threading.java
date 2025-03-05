b950d5f59cdd690ca4e17c1b9b683528d8ffb72a
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-03-05 17:31:16 +0530

    Add Threading class

---
+++ BroCode_Java/Threading.java
@@ -0,0 +1,11 @@
+public class Threading {
+    public static void main(String[] args) {
+        // Threading -> allows program to run multiple tasks simultaneously
+        // helps improve performance with time-consuming operations
+        // (File I/O, network communications, or any background tasks)
+
+        // how to create a Thread
+        // Opt1 -> Extend the Thread class
+        // Opt2 -> Implement the Runnable interface
+    }
+}
